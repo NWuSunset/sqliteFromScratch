ff07e110270f1e1c0caa7d0e4e6b9e66894fd475
BuildTools <unconfigured@null.spigotmc.org>
2025-07-21 20:27:31 -0700

    first

---
+++ main.c
@@ -0,0 +1,141 @@
+/*
+ * Database from scratch modeled after sqlite
+ *  https://www.sqlite.org/arch.html architecture of sqlite
+ */
+
+
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+//enum result codes (functions like exception as C doesn't support them)
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
+
+typedef struct {
+    char* buffer; //buffer input like .exit
+    size_t buffer_length; //length in bytes
+    ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer)); //allocate space for input buffer (casts returned pointer to InputBuffer*)
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+void print_prompt() {
+    printf("db > ");
+}
+
+void read_input(InputBuffer* input_buffer) {
+    //self note: ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+
+    //get the number of bytes read (includes newline (\n) character)
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin); //stdin is standard input stream (standard input source)
+
+    if (bytes_read <= 0 ) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //ignore the trailing newline (\n)
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0; //\n becomes a \0. Null chr
+}
+
+//free allocated buffer mem
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) { //insert will be followed by data so limit the size
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+    switch (statement->type) {
+        case (STATEMENT_INSERT):
+            printf("Insert Called. \n");
+            break;
+        case (STATEMENT_SELECT):
+            printf("Select CALLEd. \n");
+            break;
+    }
+}
+
+//reading the user input for CLI
+int main(int argc, char* argv[]) {
+    InputBuffer* input_buffer = new_input_buffer();
+
+    //accepting inputs
+    while (true) {
+        print_prompt();
+        read_input(input_buffer);
+
+        //if this is a dot/meta-command
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer)) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+                    continue;
+            }
+        }
+        //test asd asdasas sdfsdf
+        if (true) {}
+
+        Statement statement;
+        switch  (prepare_statement(input_buffer, &statement)) {
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+                continue;
+        }
+
+        execute_statement(&statement);
+        printf("Executed.\n");
+    }
+}
